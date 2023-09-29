bfff753271e40cc5a87520758355eed43c361ac3
dllchun <wingchuncheung0108@gmail.com>
2023-09-29 15:39:27 +0800

    update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,23 @@
 function ListGroup() {
-    return <ul className="list-group">
-        <li className="list-group-item active">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-    </ul>
+  let item = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {item.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {item.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default ListGroup
+export default ListGroup;
