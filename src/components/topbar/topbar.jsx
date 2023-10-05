09c469c5f7ef4ad7bedb7e0fed690902a62b3242
dllchun <wingchuncheung0108@gmail.com>
2023-10-05 15:05:09 +0800

    first commit

---
+++ src/components/topbar/topbar.jsx
@@ -0,0 +1,30 @@
+import './topbar.css'
+import {Search, Person} from '@mui/icons-material'
+
+export default function Topbar() {
+  return (
+    <div className='topbarContainer'>
+        <div className="topbarLeft">
+          <span className="logo">Vincentsocial</span>
+        </div>
+        <div className="topbarCenter">
+          <div className="searchBar">
+            <Search/>
+            <input placeholder='Search for friend, post or video' className="searchInput" />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcon">
+            <div className="topBarIconItem">
+              <Person/>
+              <span className="topbarIcon">1</span>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topbar from '../../components/topbar/topbar'
+
+
+export default function Home() {
+  return (<div>
+    <Topbar/>
+    <div>this is home page</div>
+    
+    </div>
+  )
+}
