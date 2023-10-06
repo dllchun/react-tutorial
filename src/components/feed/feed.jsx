fc2e554a0fca6b7e1405c1578dea0eb0b3d645b9
dllchun <wingchuncheung0108@gmail.com>
2023-10-06 18:37:29 +0800

    full ui

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./feed.css";
 import Share from "../share/share";
+import Post from "../post/post";
 
 export default function Feed() {
   return (
     <div className="feedContainer">
       <div className="feedWrapper">
         <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -2,5 +2,64 @@ import React from "react";
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightBarContainer">this is rightbar</div>;
+  return (
+    <div className="rightBarContainer">
+      <div className="rightBarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>vincent</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightBarAd" />
+        <h4 className="rightBarTitle">Online Friends</h4>
+        <ul className="rightBarFriendList">
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg  "
+                alt=""
+                className="rightBarProfileImg"
+              />
+              <span className="rightBarOnline"></span>
+            </div>
+            <span className="rightBarUserName">John Carter</span>
+          </li>
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg  "
+                alt=""
+                className="rightBarProfileImg"
+              />
+              <span className="rightBarOnline"></span>
+            </div>
+            <span className="rightBarUserName">John Carter</span>
+          </li>
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg  "
+                alt=""
+                className="rightBarProfileImg"
+              />
+              <span className="rightBarOnline"></span>
+            </div>
+            <span className="rightBarUserName">John Carter</span>
+          </li>
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg  "
+                alt=""
+                className="rightBarProfileImg"
+              />
+              <span className="rightBarOnline"></span>
+            </div>
+            <span className="rightBarUserName">John Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
