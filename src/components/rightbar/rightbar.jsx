657d6a3677f3ecb895224a77a53384e06de71e77
dllchun <wingchuncheung0108@gmail.com>
2023-10-07 12:24:29 +0800

    day 3 - update right bar

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -2,6 +2,34 @@ import React from "react";
 import "./rightbar.css";
 
 export default function Rightbar() {
+  const Friends = [
+    {
+      id: 1,
+      name: "Vincent Cheung",
+      img: "/assets/person/3.jpeg",
+      online: true,
+    },
+    { id: 2, name: "Kim Mak", img: "/assets/person/4.jpeg", online: true },
+    { id: 3, name: "Cherry Tang", img: "/assets/person/5.jpeg", online: false },
+    { id: 4, name: "Kawing Wong", img: "/assets/person/6.jpeg", online: true },
+  ];
+
+  const Friend = (props) => {
+    return (
+      <li className="rightBarFriend">
+        <div className="rightBarProfileImgContainer">
+          <img
+            src={props.img}
+            alt={props.name}
+            className="rightBarProfileImg"
+          />
+          <span className="rightBarOnline"></span>
+        </div>
+        <span className="rightBarUserName">{props.name}</span>
+      </li>
+    );
+  };
+
   return (
     <div className="rightBarContainer">
       <div className="rightBarWrapper">
@@ -14,50 +42,13 @@ export default function Rightbar() {
         <img src="/assets/ad.png" alt="" className="rightBarAd" />
         <h4 className="rightBarTitle">Online Friends</h4>
         <ul className="rightBarFriendList">
-          <li className="rightBarFriend">
-            <div className="rightBarProfileImgContainer">
-              <img
-                src="/assets/person/3.jpeg  "
-                alt=""
-                className="rightBarProfileImg"
-              />
-              <span className="rightBarOnline"></span>
-            </div>
-            <span className="rightBarUserName">John Carter</span>
-          </li>
-          <li className="rightBarFriend">
-            <div className="rightBarProfileImgContainer">
-              <img
-                src="/assets/person/3.jpeg  "
-                alt=""
-                className="rightBarProfileImg"
-              />
-              <span className="rightBarOnline"></span>
-            </div>
-            <span className="rightBarUserName">John Carter</span>
-          </li>
-          <li className="rightBarFriend">
-            <div className="rightBarProfileImgContainer">
-              <img
-                src="/assets/person/3.jpeg  "
-                alt=""
-                className="rightBarProfileImg"
-              />
-              <span className="rightBarOnline"></span>
-            </div>
-            <span className="rightBarUserName">John Carter</span>
-          </li>
-          <li className="rightBarFriend">
-            <div className="rightBarProfileImgContainer">
-              <img
-                src="/assets/person/3.jpeg  "
-                alt=""
-                className="rightBarProfileImg"
-              />
-              <span className="rightBarOnline"></span>
-            </div>
-            <span className="rightBarUserName">John Carter</span>
-          </li>
+          {Friends.map((friend) => {
+            if (friend.online) {
+              return (
+                <Friend key={friend.id} name={friend.name} img={friend.img} />
+              );
+            }
+          })}
         </ul>
       </div>
     </div>
